c97837486841432820c998b9abd36c2295b1511a
Jesper Wentzell <jesper.wentzell@gmail.com>
2024-09-15 15:48:00 +0200

    Added react-router-dom and testing it.

--- event-app/src/App.js
+++ event-app/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
@@ -8,6 +10,14 @@ function App() {
           The start of a great event scheduler!
         </p>
       </header>
+      <div className="Content">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ event-app/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <h1>
+                Welcome to the homepage of this app!
+            </h1>
+        );
+    }
+}
