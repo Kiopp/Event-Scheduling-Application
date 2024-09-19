d2b911706cac98a5bc1f0b05860fb6ddf69ccd10
Jesper Wentzell <jesper.wentzell@gmail.com>
2024-09-19 18:12:16 +0200

    Redesigned the website a little and test-added a few EventCards.

--- event-app/src/routes/PublicEvents.js
+++ event-app/src/routes/PublicEvents.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
 
+import EventCard from '../components/EventCard';
+
 class PublicEvents extends React.Component {
     render() {
         return (
-            <h1>
-                This is a public event!
-            </h1>
+            <div className='Content'>
+                <h1 className='PageTitle'>
+                    Public events
+                </h1>
+                <EventCard title='Epic Event' date='12/12/2024' startTime='12:40' endTime='13:40' description='This event will be very epic.' id='1'></EventCard>
+                <EventCard title='Christmas Eve 2024' date='24/12/2024' startTime='00:00' endTime='24:00' description='Merry christmas! Yet another year we celebrate christmas!' id='3'></EventCard>
+                <p>
+                    Work in progress...
+                </p>
+            </div>
         );
     }
 }
