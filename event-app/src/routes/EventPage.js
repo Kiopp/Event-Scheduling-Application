7d9c8b1724c6bb2bb68634df555178e6e4db63f3
Vencilo <axbe701@gmail.com>
2024-09-21 19:27:28 +0200

    Changed my api approach and fixed some issues
    Changed api approach to a local hosted api and fixed some issues. Application now runs but do not know if EventPage and api works properly.

--- event-app/src/routes/EventPage.js
+++ event-app/src/routes/EventPage.js
@@ -1,37 +1,30 @@
-import * as React from 'react';
+import React, { useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-class EventPage extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            event: {}
-        };
-    }
-
-    componentDidMount() {
-        const { event_id } = useParams();
-        axios.get(`/api/events/${event_id}`)
+const EventPage = () => {
+    const { event_id } = useParams();
+    const [event, setEvent] = useState(null);
+  
+    useEffect(() => {
+      axios.get(`/api/events/${event_id}`)
         .then(response => {
-            this.setState({event: response.data});
+          setEvent(response.data);
         })
         .catch(error => {
-            console.error(error);
+          console.error(error);
         });
-    }
-    render() {
-        const { event } = this.state;
-        return (
-            <div>
-                <h1>{event.title}</h1>
-                <p><strong>Date:</strong>{event.date}</p>
-                <p><strong>Start Time</strong>{event.startTime}</p>
-                <p><strong>End Time</strong>{event.endTime}</p>
-                <p><strong>Description</strong>{event.description}</p>
-            </div>
-        );
-    }
+    }, [event_id]);
+
+    return (
+        <div>
+            <h1>{event.title}</h1>
+            <p><strong>Date:</strong>{event.date}</p>
+            <p><strong>Start Time</strong>{event.startTime}</p>
+            <p><strong>End Time</strong>{event.endTime}</p>
+            <p><strong>Description</strong>{event.description}</p>
+        </div>
+    );   
 }
 
 export default EventPage;
