4ce121fd4ac653a0c6af8f5106ca61c6d1e3d750
Vencilo <axbe701@gmail.com>
2024-09-29 17:01:56 +0200

    Fixed datpicker
    You can now actually choose dates

--- event-app/src/components/DatePicker.js
+++ event-app/src/components/DatePicker.js
@@ -2,11 +2,17 @@ import * as React from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { TextField } from '@mui/material';
 
-export default function StaticDatePickerLandscape() {
+export default function StaticDatePickerLandscape({ value, onChange }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker orientation="landscape" />
+      <StaticDatePicker
+        orientation="landscape"
+        value={value}
+        onChange={onChange}
+        renderInput={(params) => <TextField {...params} />}
+      />
     </LocalizationProvider>
   );
 }
