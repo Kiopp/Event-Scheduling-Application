7933b60ff2120d13faefe174af8cb266420c5afe
Jesper Wentzell <jesper.wentzell@gmail.com>
2024-10-07 21:14:53 +0200

    Fixed friend request sending
    Reorganized the friend request api calls to /src/model-data according to the MVC model

--- event-app/src/routes/ProfilePage.js
+++ event-app/src/routes/ProfilePage.js
@@ -4,30 +4,9 @@ import EventCard from '../components/EventCard';
 import { Button, Grid2 } from '@mui/material';
 import { CircularProgress } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import { handleAddFriendButtonClick } from '../model-data/FriendData';
 
 
-
-const handleAddFriendButtonClick = async (targetUserId) => {
-  try {
-    // Make POST request to backend
-    axios.post(`http://localhost:5001/api/friend-request/${targetUserId}`, { withCredentials: true })
-      .then((response) => {
-        // Update UI to notify the user of success! 
-      })
-      .catch((error) => {
-        if (error.response && error.response.status === 401) {
-          console.error('Friend request failed:', error.response);
-        }
-      });
-  } catch (error) {
-    // Check if the response contains a message
-    if (error.response && error.response.data) {
-        console.error('Friend request failed:', error.response.data.message);
-    } else {
-        console.error('Friend request failed:', error);
-    }
-  }
-};
 
 function ProfilePage() {
   const { userId } = useParams();
