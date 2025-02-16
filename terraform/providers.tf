58f848dfb8d191f13246e5b3da83730d5c6fbc22
Vencilo <165160198+Vencilo@users.noreply.github.com>
2025-02-16 15:59:44 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,12 +3,3 @@ provider "azurerm" {
 }
 
 provider "azuread" {}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-}
