42c00a186e0f13c73f972c469b7d651d0e25bfeb
Vencilo <165160198+Vencilo@users.noreply.github.com>
2025-02-16 15:43:36 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,36 +1,12 @@
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-  default     = "DESAP"
+provider "azurerm" {
+  features {}
 }
 
-variable "location" {
-  description = "Azure region for the resources"
-  type        = string
-  default     = "Sweden"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
-
-variable "aks_cluster_name" {
-  description = "AKS cluster name"
-  type        = string
-  default     = "Clusterfuck"
-}
-
-variable "node_count" {
-  description = "Number of nodes in the AKS node pool"
-  type        = number
-  default     = 2
-}
-
-variable "node_vm_size" {
-  description = "VM size for the AKS nodes"
-  type        = string
-  default     = "Standard_DS2_v2"
-}
-
-variable "acr_name" {
-  description = "Azure Container Registry name"
-  type        = string
-  default     = "Bombaclat"
-}
-
