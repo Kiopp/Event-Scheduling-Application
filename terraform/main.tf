154590322abab054944632d302a3a99c1f68247a
Vencilo <165160198+Vencilo@users.noreply.github.com>
2025-02-19 17:06:28 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-# Create the DESAP Resource Group in Sweden Central
-resource "azurerm_resource_group" "desap" {
+# Create the Resource Group in Sweden Central
+resource "azurerm_resource_group" "rg" {
   name     = "DESAP"
   location = "Sweden Central"
 }
@@ -17,52 +17,30 @@ data "azuread_user" "weje22wy" {
   user_principal_name = "weje22wy@student.ju.se"
 }
 
-# Assign the Owner role to each user on the DESAP Resource Group
+# Assign the Owner role (highest privilege) to each user on the Resource Group
 resource "azurerm_role_assignment" "noca22tf_assignment" {
-  scope                = azurerm_resource_group.desap.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "Owner"
   principal_id         = data.azuread_user.noca22tf.object_id
 }
 
 resource "azurerm_role_assignment" "beax22tr_assignment" {
-  scope                = azurerm_resource_group.desap.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "Owner"
   principal_id         = data.azuread_user.beax22tr.object_id
 }
 
 resource "azurerm_role_assignment" "weje22wy_assignment" {
-  scope                = azurerm_resource_group.desap.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "Owner"
   principal_id         = data.azuread_user.weje22wy.object_id
 }
 
-# Create a Container Repository (Azure Container Registry) in the DESAP Resource Group
+# Create the Container Repository (Azure Container Registry) in the Resource Group
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.desap.name
-  location            = azurerm_resource_group.desap.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
-}
-
-# Create an AKS cluster in the DESAP Resource Group
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  location            = azurerm_resource_group.desap.location
-  resource_group_name = azurerm_resource_group.desap.name
-  dns_prefix          = "${var.aks_cluster_name}-dns"
-
-  default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_vm_size
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Development"
-  }
 }
