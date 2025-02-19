fd1aa520a4116ca1cc1ac19994e6942d43497390
Vencilo <165160198+Vencilo@users.noreply.github.com>
2025-02-19 17:04:29 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,6 +55,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
+    vm_size    = var.node_vm_size
   }
 
   identity {
